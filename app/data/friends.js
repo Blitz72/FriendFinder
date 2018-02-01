8a0ea6d7a9bed0470760e38d199a180ea65e7342
David Bauer <bauer.david@att.net>
2018-02-01 13:52:31 -0500

    working commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,47 +6,47 @@ var friendsList = [
 	},
 	{
 		name: "John",
-		photo: "https://i.ytimg.com/vi/tldi7Zz8DRs/maxresdefault.jpg",
+		photo: "https://media.indiatimes.in/media/content/itimes/photo/2013/Sep/30/1380535527-mr-bean-photoshopped-512620a04d490.jpg",
 		answers: [1, 4, 3, 5, 2, 5, 5, 4, 3, 2]
 	},
 	{
-		name: "Debbie",
-		photo: "https://i.ytimg.com/vi/tldi7Zz8DRs/maxresdefault.jpg",
+		name: "Caroline",
+		photo: "http://imgs.mastphotos.com/wp-content/uploads/2014/02/Mr.-Bean-Turned-Into-A-Lady.jpg",
 		answers: [3, 5, 2, 1, 4, 3, 3, 5, 1, 1]
 	},
 	{
 		name: "Steve",
-		photo: "https://i.ytimg.com/vi/tldi7Zz8DRs/maxresdefault.jpg",
+		photo: "http://famepace.com/wp-content/uploads/2017/09/Funny-Mr-Bean-Photoshop-13.jpg",
 		answers: [4, 4, 5, 5, 1, 2, 4, 5, 3, 3]
 	},
 	{
 		name: "George",
-		photo: "https://i.ytimg.com/vi/tldi7Zz8DRs/maxresdefault.jpg",
+		photo: "http://photos1.blogger.com/blogger/816/3234/1600/photoshopCel.jpg",
 		answers: [3, 3, 4, 2, 2, 4, 5, 2, 3, 4]
 	},
 	{
 		name: "Heather",
-		photo: "https://i.ytimg.com/vi/tldi7Zz8DRs/maxresdefault.jpg",
+		photo: "http://viralslot.com/wp-content/uploads/2017/03/11-17.jpg",
 		answers: [5, 5, 5, 2, 4, 1, 5, 4, 2, 1]
 	},
 	{
 		name: "Joseph",
-		photo: "https://i.ytimg.com/vi/tldi7Zz8DRs/maxresdefault.jpg",
+		photo: "https://www.sunnyskyz.com/uploads/2017/03/rycm2-mr-bean-photoshop-hilarious-14.jpg",
 		answers: [2, 4, 5, 1, 3, 4, 2, 2, 3, 5]
 	},
 	{
-		name: "Hannah",
-		photo: "https://i.ytimg.com/vi/tldi7Zz8DRs/maxresdefault.jpg",
+		name: "Tricia",
+		photo: "https://orig00.deviantart.net/3091/f/2014/351/7/a/snooki_beanedit_by_nekothebizzare-d8a693w.jpg",
 		answers: [5, 1, 1, 3, 4, 1, 2, 5, 5, 5]
 	},
 	{
 		name: "Jack",
-		photo: "https://i.ytimg.com/vi/tldi7Zz8DRs/maxresdefault.jpg",
+		photo: "http://www.humoar.com/wp-content/uploads/photoshops/bean/bean-9.jpg",
 		answers: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
 	},
 	{
 		name: "Mitch",
-		photo: "https://i.ytimg.com/vi/tldi7Zz8DRs/maxresdefault.jpg",
+		photo: "https://www.demilked.com/magazine/wp-content/uploads/2017/03/mr-bean-rowan-atkinson-photoshop-battle-34.jpg",
 		answers: [4, 3, 3, 4, 2, 2, 2, 4, 5, 3]
 	}
 ]
